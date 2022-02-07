27e6925fc7a9c099e4773164c9e619ed211bc689
matěj <matj@DESKTOP-1HI5AKA>
2022-02-07 16:23:56 +0100

    Přidejte soubory projektu.

---
+++ Hexdump/Hexdump.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+int main() {
+	FILE* soubor = fopen("vystup.txt", "w");
+
+	if (soubor == NULL) {
+		
+		printf("Chyba: Nelze otevřít soubor pro zápis.\n");
+		return 1;
+	}
+
+	int ascii = 0x40;
+
+	fprintf(soubor, "Znak - Hex\n");
+
+	for (int i = 0; i < 20; i++) {
+		
+		ascii += 1;
+		
+		fwrite(&ascii, sizeof(int), 1, soubor);
+		fprintf(soubor, "0x%x\n", ascii);
+	}
+
+	if (fclose(soubor) == EOF) {
+		
+		printf("Chyba: Nepodařilo se otevřít soubor.\n");
+		return 1;
+	}
+
+	return(0);
+}
